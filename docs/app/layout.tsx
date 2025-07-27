130f0955b3684ffa702967e632a1ec7982d057f5
Stout Ni <stout.officejob@gmail.com>
2025-07-27 12:55:51 +0800

    chore: check before commit

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -1,22 +1,27 @@
-import type { Metadata } from "next";
-import { Head } from "nextra/components";
-import { getPageMap } from "nextra/page-map";
-import { Footer, Layout, Navbar } from "nextra-theme-docs";
-import "nextra-theme-docs/style.css";
+import type { Metadata } from 'next';
+import { Head } from 'nextra/components';
+import { getPageMap } from 'nextra/page-map';
+import { Footer, Layout, Navbar } from 'nextra-theme-docs';
+import 'nextra-theme-docs/style.css';
 
 export const metadata: Metadata = {
-  generator: "Next.js",
-  applicationName: "FolderKit",
+  generator: 'Next.js',
+  applicationName: 'FolderKit',
   appleWebApp: {
-    title: "FolderKit",
+    title: 'FolderKit',
   },
   title: {
-    default: "FolderKit: Pixel-Perfect MacOS Folder Icons Generator",
-    template: "%s | FolderKit",
+    default: 'FolderKit: Pixel-Perfect MacOS Folder Icons Generator',
+    template: '%s | FolderKit',
   },
 };
 
-const navbar = <Navbar logo={<b>FolderKit</b>} projectLink="https://github.com/stout-ni/folderkit" />;
+const navbar = (
+  <Navbar
+    logo={<b>FolderKit</b>}
+    projectLink="https://github.com/stout-ni/folderkit"
+  />
+);
 const footer = <Footer>MIT {new Date().getFullYear()} © FolderKit.</Footer>;
 
 export default async ({ children }: { children: React.ReactNode }) => (
